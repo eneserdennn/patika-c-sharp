7721c04034b191006621f386ef276d731079b1c0
Enes Erden <eneserdendev@gmail.com>
2023-10-17 00:40:14 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Isminizi giriniz");
+string name = Console.ReadLine() ?? "";
+Console.WriteLine("Soyadinizi giriniz");
+string surname = Console.ReadLine() ?? "";
+Console.WriteLine("Merhaba" + " " + name + " " + surname);
